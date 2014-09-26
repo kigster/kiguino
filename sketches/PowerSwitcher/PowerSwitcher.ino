b26adc34f4a1cf14155764e260210e61beda3c69
Konstantin Gredeskoul <kigster@gmail.com>
2014-09-25 20:54:19 -0700

    Add LED to PowerSwitcher

--- sketches/PowerSwitcher/PowerSwitcher.ino
+++ sketches/PowerSwitcher/PowerSwitcher.ino
@@ -1,29 +1,19 @@
-#include "printf.h"
-
-#include "Sonar.h"
-#include "MotionSensor.h"
-
+#include <Arduino.h>
 #include <SimpleTimer.h>
+#include <MotionSensor.h>
+
+#include "printf.h"
 
 // #define DEBUG
 
 uint8_t	pinIRInput 		= 11,
-		pinSonarTrigger = 9,
-		pinSonarEcho 	= 10,
 		pinSwitch 		= 13,
-		pinButton		= 8
+		pinButton		= 8,
+		pinLed 			= 6
 		;
-
-Sonar sonar(
-		pinSonarTrigger,
-		pinSonarEcho,
-		500,				   // Maximum distance we want to ping for (in centimeters).
-			  	  	  	  	   // Maximum sensor distance is rated at 400-500cm.
-		100);   // Recognize objects within this many centimeters
 
 MotionSensor motion(pinIRInput, 5000);
 SimpleTimer timer(1);
-
 
 const uint16_t INACTIVITY_THRESHOLD = 120;
 
@@ -32,14 +22,13 @@ char buffer[90];
 typedef struct StateStruct {
 	bool active;
 	bool motionDetected;
-	bool sonarDetected;
 	bool buttonPressed;
-	unsigned long sonarDistanceDetected;
 	uint32_t lastActivityAt;
 	uint32_t lastStateChangeAt;
 } CurrentState;
 
 CurrentState state;
+bool ledOn;
 
 void power(bool on) {
 	digitalWrite(pinSwitch, on ? HIGH : LOW);
@@ -60,28 +49,26 @@ void detectMotion(int timerId) {
 	}
 }
 
-void detectSonar(int timerId) {
-	bool prev = state.sonarDetected;
-	state.sonarDetected = sonar.detected();
-	if (state.sonarDetected) {
-#ifdef DEBUG
-		if (!prev) printf("Detected sonar! Distance is %d\n", sonar.getDistance());
-#endif
-		state.sonarDistanceDetected = sonar.getDistance();
-		state.lastActivityAt = millis();
+void showLed(int timerId) {
+	if (state.active && !state.motionDetected) {
+		ledOn = !ledOn;
+	} else {
+		ledOn = false;
 	}
+
+	digitalWrite(pinLed, ledOn ? HIGH : LOW);
 }
 
 void switchPower(int timerId) {
 	if (state.active) {
 		if ((!state.buttonPressed) ||
-			(!state.motionDetected && !state.sonarDetected && (millis() - state.lastActivityAt) / 1000 > INACTIVITY_THRESHOLD)
+			(!state.motionDetected && (millis() - state.lastActivityAt) / 1000 > INACTIVITY_THRESHOLD)
 			) {
 			state.lastStateChangeAt = millis();
 			state.active = false;
 		}
 	} else {
-		if (state.buttonPressed && (state.motionDetected || state.sonarDetected)) {
+		if (state.buttonPressed && state.motionDetected) {
 			state.active = true;
 			state.lastStateChangeAt = millis();
 		}
@@ -92,16 +79,13 @@ void switchPower(int timerId) {
 
 void logCurrentState() {
 
-	printf("Active? %s | Button %s | Motion? %s | Sonar? %s (%4d/%4d)",
+	printf("Active? %s | Button %s | Motion? %s",
 			(state.active ? "YES" : " NO"),
 			(state.buttonPressed ? " ON" : "OFF"),
-			(state.motionDetected ? "YES" : " NO"),
-			(state.sonarDetected  ? "YES" : " NO"),
-				sonar.getDistance(),
-				sonar.getDistanceThreshold()
+			(state.motionDetected ? "YES" : " NO")
   			);
 
-	if (state.active && state.buttonPressed && !state.motionDetected && !state.sonarDetected) {
+	if (state.active && state.buttonPressed && !state.motionDetected) {
 		long timeLeft = INACTIVITY_THRESHOLD - (millis() - state.lastActivityAt) / 1000;
 		if (timeLeft < 0) timeLeft = 0;
 		printf(" | Time Remaining? (%d/%d) sec ",
@@ -118,6 +102,7 @@ void setup(void) {
 	memset(&state, 0x0, sizeof(StateStruct));
 
 	pinMode(pinSwitch, OUTPUT);
+	pinMode(pinLed, OUTPUT);
 	pinMode(pinButton, INPUT);
 	pinMode(pinIRInput, INPUT);
 
@@ -129,9 +114,9 @@ void setup(void) {
 	motion.init(5000);
 
 	timer.setInterval( 110,  &detectMotion);
-	timer.setInterval( 150,  &detectSonar);
 	timer.setInterval( 100,  &detectButtonPressed);
 	timer.setInterval(1000,  &switchPower);
+	timer.setInterval( 500,  &showLed);
 }
 
 
