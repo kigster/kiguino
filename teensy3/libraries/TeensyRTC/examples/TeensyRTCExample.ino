95850454bcd1941a2e408ff11fcfdba151c50799
Konstantin Gredeskoul <kigster@gmail.com>
2014-11-13 21:20:39 -0800

    Adding example

---
+++ teensy3/libraries/TeensyRTC/examples/TeensyRTCExample.ino
@@ -0,0 +1,40 @@
+/*
+ * TeensyRTCExample
+ *
+ *  Created on: Nov 13, 2014
+ *      Author: Konstantin Gredeskoul
+ *        Code: https://github.com/kigster
+ *
+ *  (c) 2014 All rights reserved, MIT License.
+ */
+
+#include <Time.h>
+#include <TeensyRTC.h>
+
+TeensyRTC rtc;
+
+char stringBuffer[20];
+
+time_t getTeensy3Time() {
+	return Teensy3Clock.get();
+}
+
+void logTime() {
+	rtc.formatTime(stringBuffer, rtc.currentTime());
+	Serial.print("Current Time: ");
+	Serial.println(stringBuffer);
+}
+
+void setup() {
+	setSyncProvider(getTeensy3Time);
+
+	Serial.begin(115200);
+	Serial.println("Starting sketch TeensyRTCExample");
+
+	rtc.begin();
+}
+
+void loop() {
+    logTime();
+    delay(1000);
+}
